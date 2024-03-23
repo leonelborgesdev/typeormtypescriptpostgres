8c6791c886fd1453a8efff1da764c974a12c07b7
leonelborgesdev <leonel03t@gmail.com>
2024-03-22 21:07:00 -0400

    getUserscontrollers

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from "express";
 import { User } from "../entities/User";
 
+export const getUsers=async(req:Request, res:Response)=>{
+    try {
+        const users=await User.find();
+        return res.json(users)
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(500).json({message: error.message})
+        }
+    }
+}
+
 export const createUser=async(req:Request, res:Response)=>{
     const {firstname, lastname}= req.body;
 
