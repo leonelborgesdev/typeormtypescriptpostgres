2f0cb63e2a9b7d5690b0f94aab02664148f246f8
leonelborgesdev <leonel03t@gmail.com>
2024-03-22 20:52:36 -0400

    post user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from "express";
+import { User } from "../entities/User";
 
-export const createUser=(req:Request, res:Response)=>{
+export const createUser=async(req:Request, res:Response)=>{
+    const {firstname, lastname}= req.body;
+
+    const user=new User()
+    user.firstname=firstname;
+    user.lastname=lastname;
+
+    await user.save()
+    console.log(user)
     res.send("hello world")
 }
