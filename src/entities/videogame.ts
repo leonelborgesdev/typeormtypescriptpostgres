9248cb4e937e4e3c41cf5c3042fd24f75417ee54
leonelborgesdev <leonel03t@gmail.com>
2024-03-24 22:36:09 -0400

    clase videogame

---
+++ src/entities/videogame.ts
@@ -0,0 +1,14 @@
+export class videogame{
+    
+    ID: number
+
+    Nombre : string
+
+    Descripción : string
+
+    Fecha_lanzamiento : string
+
+    Rating : string
+    
+    Plataformas : string
+}
