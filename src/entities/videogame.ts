fb10d29d191ec087ed208a78f5c612b7078bcc1c
leonelborgesdev <leonel03t@gmail.com>
2024-03-24 22:58:05 -0400

    videogame entity

--- src/entities/videogame.ts
+++ src/entities/videogame.ts
@@ -1,14 +1,32 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm"
+
+@Entity()
 export class videogame{
     
+    @PrimaryColumn()
     ID: number
 
+    @Column()
     Nombre : string
 
+    @Column()
+    Image : string
+
+    @Column()
     Descripción : string
 
+    @Column()
     Fecha_lanzamiento : string
 
+    @Column()
     Rating : string
-    
+
+    @Column()
     Plataformas : string
+    
+    @CreateDateColumn()
+    createAt: Date
+
+    @UpdateDateColumn()
+    updateAt: Date
 }
